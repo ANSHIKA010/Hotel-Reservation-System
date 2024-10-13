ac0960e7a64280d7da84e2e1ff348001b28647c4
Anshika <sanshikas010@gmail.com>
2024-10-13 22:07:42 +0530

    ReserveRoom function made.

--- src/Main.java
+++ src/Main.java
@@ -36,14 +36,67 @@ public class Main {
 
                 switch (choice) {
                     case 1:
-
+                        reserveRoom(connection, scanner);
+                        break;
+//                    case 2:
+//                        viewReservations(connection);
+//                        break;
+//                    case 3:
+//                        getRoomNumber(connection, scanner);
+//                        break;
+//                    case 4:
+//                        updateReservation(connection, scanner);
+//                        break;
+//                    case 5:
+//                        deleteReservation(connection, scanner);
+//                        break;
+//                    case 0:
+//                        exit();
+//                        scanner.close();
+//                        return;
+                    default:
+                        System.out.println("Invalid choice. Try again.");
                 }
 
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+        private static void reserveRoom(Connection connection, Scanner scanner) {
+            try {
+                System.out.print("Enter guest name: ");
+                String guestName = scanner.next();
+                scanner.nextLine();
+                System.out.print("Enter room number: ");
+                int roomNumber = scanner.nextInt();
+                System.out.print("Enter contact number: ");
+                String contactNumber = scanner.next();
+
+                String sql = "INSERT INTO reservation(guest_name, room_number, contact_number) " +
+                        "VALUES ('" + guestName + "', " + roomNumber + ", '" + contactNumber + "')";
+
+                try (Statement statement = connection.createStatement()) {
+                    int affectedRows = statement.executeUpdate(sql);
+
+                    if (affectedRows > 0) {
+                        System.out.println("Reservation successful!");
+                    } else {
+                        System.out.println("Reservation failed.");
+                    }
+                }
+            }
+                catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
+
+
 
     }
-}
